22156ab5d02b3c6111447c6cefda36319ccce59f
unknown <semir.muratovic1990@hotmail.com>
2024-09-28 22:27:41 +0200

    New user dialog modified

--- src/app/user/dialog-add-user/dialog-add-user.component.ts
+++ src/app/user/dialog-add-user/dialog-add-user.component.ts
@@ -11,11 +11,15 @@ import {
   MatDialogTitle,
 } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
+  providers: [provideNativeDateAdapter()],
   imports: [
     MatFormFieldModule,
     MatInputModule,
@@ -25,6 +29,8 @@ import { MatInputModule } from '@angular/material/input';
     MatDialogContent,
     MatDialogActions,
     MatDialogClose,
+    MatIconModule,
+    MatDatepickerModule,
   ],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss',
