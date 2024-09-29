75ed88f772010219f1843cb27fd5e4f1fb892ac0
unknown <semir.muratovic1990@hotmail.com>
2024-09-29 11:08:34 +0200

    Firebase firestore database implemented

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,23 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'simple-crm-8ecea',
+        appId: '1:1098550376514:web:6c4c3e0c0af67e315715db',
+        storageBucket: 'simple-crm-8ecea.appspot.com',
+        apiKey: 'AIzaSyCnHOTH_Yh-pMvmsau23bcIlrrdTEbP5h4',
+        authDomain: 'simple-crm-8ecea.firebaseapp.com',
+        messagingSenderId: '1098550376514',
+      })
+    ),
+    provideFirestore(() => getFirestore()),
+  ],
 };
--- src/app/user/dialog-add-user/dialog-add-user.component.ts
+++ src/app/user/dialog-add-user/dialog-add-user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import {
@@ -16,6 +16,15 @@ import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { User } from '../../../models/user.class';
+import {
+  Firestore,
+  collection,
+  collectionData,
+  doc,
+  setDoc,
+  addDoc,
+} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-dialog-add-user',
@@ -37,11 +46,27 @@ import { User } from '../../../models/user.class';
   styleUrl: './dialog-add-user.component.scss',
 })
 export class DialogAddUserComponent {
+  firestore: Firestore = inject(Firestore);
   user = new User();
   birthDate!: Date;
+
+  items$;
+
+  constructor() {
+    const itemCollection = collection(this.firestore, 'users');
+    this.items$ = collectionData(itemCollection);
+  }
 
   save() {
     this.user.birthDate = this.birthDate.getTime();
     console.log(this.user);
+    const ref = collection(this.firestore, 'users');
+    addDoc(ref, this.user.toJson())
+      .catch((err) => {
+        console.error('Failed to set new doc!', err);
+      })
+      .then((result) => {
+        console.log('Adding user finished', result);
+      });
   }
 }
